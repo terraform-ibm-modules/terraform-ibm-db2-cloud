ddd16c691c66f4b8ce3d0adb867775eaec9dd029
Matthew.Lemmond@ibm.com <Matthew.Lemmond@ibm.com>
2024-11-27 11:50:40 -0500

    refactor: pre-commit error in pipeline

--- examples/basic/version.tf
+++ examples/basic/version.tf
@@ -7,9 +7,6 @@ terraform {
     ibm = {
       source  = "IBM-Cloud/ibm"
       version = "1.72.0-beta0"
-      # currently locked into version while in beta release stage
-      # uncomment the version line below after beta is complete
-      # version = ">= 1.72.0, < 2.0.0"
     }
   }
 }
