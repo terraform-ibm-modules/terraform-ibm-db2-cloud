de0020c4c7dd4238ad6e7c60134438d5932b01e1
Matthew.Lemmond@ibm.com <Matthew.Lemmond@ibm.com>
2024-12-05 09:31:51 -0500

    refactor: modify plan/version

--- main.tf
+++ main.tf
@@ -6,14 +6,14 @@ resource "ibm_db2" "db2" {
   name              = var.db2_instance_name
   resource_group_id = var.resource_group_id
   location          = "us-south"
-  plan              = "free"
+  plan              = "performance-preprod"
   service           = "dashdb-for-transactions"
   high_availability = var.enable_high_availability ? "yes" : "no"
   service_endpoints = var.service_endpoints
 
   parameters = {
     "oracle_compatibility" : var.enable_oracle_compatibility ? "yes" : "no",
-    # "version" : "12",
+    "version" : "12",
     "node_type" : var.node_type
   }
 
