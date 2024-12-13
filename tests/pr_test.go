9cf50584416dc6a24481c0e328f41d70bc0ef94b
Matthew.Lemmond@ibm.com <Matthew.Lemmond@ibm.com>
2024-12-13 10:32:20 -0500

    refactor: update tests

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -2,15 +2,18 @@
 package test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/testhelper"
+	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/testschematic"
 )
 
 // Use existing resource group
 const resourceGroup = "geretain-test-resources"
 const advancedExampleDir = "examples/advanced"
+const solutionDir = "solutions/standard"
 
 func setupOptions(t *testing.T, prefix string, dir string) *testhelper.TestOptions {
 	options := testhelper.TestOptionsDefaultWithVars(&testhelper.TestOptions{
@@ -39,6 +42,60 @@ func TestRunUpgradeExample(t *testing.T) {
 	t.Parallel()
 
 	options := setupOptions(t, "adv-upg", advancedExampleDir)
+
+	output, err := options.RunTestUpgrade()
+	if !options.UpgradeTestSkipped {
+		assert.Nil(t, err, "This should not have errored")
+		assert.NotNil(t, output, "Expected some output")
+	}
+}
+
+func setupDAOptions(t *testing.T, prefix string, dir string) *testhelper.TestOptions {
+	options := testhelper.TestOptionsDefault(&testhelper.TestOptions{
+		Testing:      t,
+		TerraformDir: dir,
+		Prefix:       prefix,
+	})
+
+	options.TerraformVars = map[string]interface{}{
+		"provider_visibility":         "public",
+		"prefix":                      options.Prefix,
+		"db2_instance_name":           "db2-da",
+		"resource_group_name":         resourceGroup,
+		"use_existing_resource_group": true,
+	}
+
+	return options
+}
+
+func TestRunDASchematics(t *testing.T) {
+	options := testschematic.TestSchematicOptionsDefault(&testschematic.TestSchematicOptions{
+		Testing:                t,
+		TarIncludePatterns:     []string{"*.tf", fmt.Sprintf("%s/*.tf", solutionDir)},
+		TemplateFolder:         solutionDir,
+		ResourceGroup:          resourceGroup,
+		Prefix:                 "db2-schem",
+		Tags:                   []string{"test-schematic"},
+		DeleteWorkspaceOnFail:  false,
+		WaitJobCompleteMinutes: 60,
+	})
+
+	options.TerraformVars = []testschematic.TestSchematicTerraformVar{
+		{Name: "ibmcloud_api_key", Value: options.RequiredEnvironmentVars["TF_VAR_ibmcloud_api_key"], DataType: "string", Secure: true},
+		{Name: "prefix", Value: options.Prefix, DataType: "string"},
+		{Name: "db2_instance_name", Value: "standard-db2", DataType: "string"},
+		{Name: "resource_group_name", Value: resourceGroup, DataType: "string"},
+		{Name: "use_existing_resource_group", Value: true, DataType: "boolean"},
+	}
+
+	err := options.RunSchematicTest()
+	assert.NoError(t, err, "Schematic Test had unexpected error")
+}
+
+func TestRunUpgradeDA(t *testing.T) {
+	t.Parallel()
+
+	options := setupDAOptions(t, "db2-upg", solutionDir)
 
 	output, err := options.RunTestUpgrade()
 	if !options.UpgradeTestSkipped {
