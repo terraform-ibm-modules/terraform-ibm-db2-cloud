333d3bb094c2d92cf24434a9f1a6494ff9bb60cb
valerio-bontempi <valerio.bontempi@it.ibm.com>
2025-06-05 17:54:55 +0200

    chore: some adjustments

--- solutions/fully-configurable/variables.tf
+++ solutions/fully-configurable/variables.tf
@@ -33,7 +33,6 @@ variable "region" {
 variable "prefix" {
   type        = string
   description = "The prefix to add to all resources that this solution creates. To not use any prefix value, you can set this value to `null` or an empty string."
-  nullable    = true
 }
 
 variable "existing_resource_group_name" {
