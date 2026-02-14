f3c1112e5d3e3e20c053c47301cfc69bb366b2be
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-02-14 07:20:19 +0000

    fix(deps): update dependencies (#120)

--- solutions/fully-configurable/main.tf
+++ solutions/fully-configurable/main.tf
@@ -4,14 +4,14 @@
 
 module "resource_group" {
   source                       = "terraform-ibm-modules/resource-group/ibm"
-  version                      = "1.4.7"
+  version                      = "1.4.8"
   existing_resource_group_name = var.existing_resource_group_name
 }
 
 module "crn_parser_subid" {
   count   = var.subscription_id_secret_crn != null ? 1 : 0
   source  = "terraform-ibm-modules/common-utilities/ibm//modules/crn-parser"
-  version = "1.4.1"
+  version = "1.4.2"
   crn     = var.subscription_id_secret_crn
 }
 
