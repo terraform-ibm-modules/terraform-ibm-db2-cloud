5019f75187e6fcc2172b4e1065347f3de8da4ed7
Valerio Bontempi <valerio.bontempi@it.ibm.com>
2026-07-10 23:02:05 +0200

    fix: fixed renovate deps

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -4,7 +4,7 @@
 
 module "resource_group" {
   source  = "terraform-ibm-modules/resource-group/ibm"
-  version = "1.4.8"
+  version = "1.6.1"
   # if an existing resource group is not set (null) create a new one using prefix
   resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
@@ -22,5 +22,5 @@ module "db2_instance" {
   enable_high_availability    = true
   enable_oracle_compatibility = var.oracle_compatibility
   node_type                   = "bx2.1x4"
-  tags                        = var.resource_tags
+  resource_tags               = var.resource_tags
 }
--- solutions/fully-configurable/variables.tf
+++ solutions/fully-configurable/variables.tf
@@ -82,7 +82,7 @@ variable "service_endpoints" {
 
 variable "resource_tags" {
   type        = list(string)
-  description = "Optional list of tags to be added to created resources"
+  description = "Add user resource tags to the Db2 Cloud instance to organize, track, and manage costs. [Learn more](https://cloud.ibm.com/docs/account?topic=account-tag&interface=ui#tag-types)."
   default     = []
 }
 
